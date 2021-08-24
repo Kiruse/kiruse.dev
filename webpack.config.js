d9388e6748dd10b89cd4e76ff91f55f5b86bb925
Kiruse <26940742+Kiruse@users.noreply.github.com>
2021-08-24 22:03:55 +0200

    webpack for bundled JS

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////
+// webpack config to bundle js
+// single bundle.js essentially necessary for IPFS
+const path = require('path')
+
+module.exports = {
+    entry: './assets/scripts/dist/index.js',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, './assets/scripts/dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './assets/scripts/dist'),
+        },
+    },
+    devtool: 'source-map',
+}
