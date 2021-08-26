6783b8713ab1342d1ac83eb7bceb96bbd726e6e7
Kiruse <26940742+Kiruse@users.noreply.github.com>
2021-08-27 00:33:51 +0200

    moved cors header to start otherwise rejected responses are denied by browser

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -36,6 +36,8 @@ const mailTransport = nodemailer.createTransport({
 const rxEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 export const sendmail = functions.region('europe-west1').https.onRequest(async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', allowedOrigin(req.headers.origin));
+    
     if (!req.accepts('application/x-www-form-urlencoded', 'multipart/form-data')) {
         res.status(406).end();
         return;
@@ -57,7 +59,6 @@ export const sendmail = functions.region('europe-west1').https.onRequest(async (
         return;
     }
     
-    res.setHeader('Access-Control-Allow-Origin', allowedOrigin(req.headers.origin));
     const msg = {
         from: 'kiruse@kiruse.com',
         sender: sender + '',
